ad1e883b97211f5e749a7f5ecd180e45f0613e7b
geonwoo0215 <gw980215@naver.com>
2023-11-26 16:51:12 +0900

    feat : 사용자 도메인 추가

---
+++ src/main/java/com/loginlogout/loginlogout/domain/Member.java
@@ -0,0 +1,17 @@
+package com.loginlogout.loginlogout.domain;
+
+public class Member {
+
+    private String id;
+
+    private String email;
+
+    private String password;
+
+    public Member(String id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+}
